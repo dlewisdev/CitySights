97596491c3bc51cdfd6e889f00d959cf1f9a7080
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-13 14:52:21 -0500

    Initial Commit

---
+++ CitySights/CitySightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsApp.swift
+//  CitySights
+//
+//  Created by Danielle Lewis on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
