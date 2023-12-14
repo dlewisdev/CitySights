d27303aa02769ffdb752f87070d7370e76d3abc5
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-14 12:13:46 -0500

    -adds business detail view

---
+++ CitySights/BusinessDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  BusinessDetailView.swift
+//  CitySights
+//
+//  Created by Danielle Lewis on 12/14/23.
+//
+
+import SwiftUI
+
+struct BusinessDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BusinessDetailView()
+}
